048813ce89eb651991634a7cdf2a610b24741b73
Josh Hall <joshhall90@yahoo.com>
2019-07-18 15:57:42 -0400

    added call to sample data ToDo: swap sample data to some test data of a given site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { withAuthenticator }  from 'aws-amplify-react'
+import { Analytics } from 'aws-amplify'
+import { API, graphqlOperation } from 'aws-amplify'
+
+const ListTodos = `
+  query {
+   listTodos{
+     items {
+       id name description completed
+     }
+   }
+  }
+`
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
+
+  async componentDidMount() {
+    const todoData = await API.graphql(graphqlOperation(ListTodos))
+    this.setState({ todos: todoData.data.listTodos.items })
+  }
+  // Auth.currentAuthenticatedUser()
+  recordEvent() {
+    console.log('about to record event!')
+    Analytics.record({
+      name: 'Test Event 1',
+      attributes: {
+        username: 'jishue'
+      }
+    });
+  }
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from Amplify</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => this.recordEvent()}>Record Event</button>
       </header>
+      {
+        this.state.todos.map((todo,i) => (
+          <div>
+            <h3>{todo.name}</h3>
+            <p>{todo.description}</p>
+          </div>
+        ))
+      }
     </div>
   );
+ }
 }
 
-export default App;
+export default withAuthenticator(App);
